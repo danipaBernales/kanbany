c0a04a0165c9dc5a996f954d13708e402a55753a
Daniela Paz Bernales <dbernales.miranda@gmail.com>
2025-03-05 00:46:10 -0300

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'tasks'
 
 urlpatterns = [
     # Public URLs
     path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='tasks/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='tasks:index'), name='logout'),
+    
+    # Protected URLs
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('tasks/', views.task_list, name='task_list'),
 ]
