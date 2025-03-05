d824867e6800e9b40c4413dcc0645aba1b95a9f6
Daniela Paz Bernales <dbernales.miranda@gmail.com>
2025-03-05 00:15:10 -0300

    Delete kanbanproject/tasks directory

--- kanbanproject/tasks/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class TasksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kanbanproject.tasks'
-
-    def ready(self):
-        from . import signals
